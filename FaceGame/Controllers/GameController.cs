ff2d872756e5115181a47de3f7448c87beba7fd7
Andrey Kurosh <mail@spark.ms>
2016-09-04 20:31:41 +0300

    Game logic wire-up.

--- FaceGame/Controllers/GameController.cs
+++ FaceGame/Controllers/GameController.cs
@@ -1,13 +1,15 @@
-﻿using System.Web.Http;
+﻿using System.Web;
+using System.Web.Http;
 using FaceGame.Code;
 using FaceGame.ViewModels.Messages;
+using FaceGame.ViewModels.State;
 
 namespace FaceGame.Controllers
 {
     /// <summary>
     /// The game-handler controller.
     /// </summary>
-    [RoutePrefix("~/api/")]
+    [RoutePrefix("~/api")]
     public class GameController: ApiController
     {
         public GameController(HiscoreManager hiscoreMgr, StateManager stateMgr)
@@ -18,6 +20,21 @@ namespace FaceGame.Controllers
 
         private readonly HiscoreManager _hiscoreManager;
         private readonly StateManager _stateManager;
+
+        private StateVM State => HttpContext.Current.Session[nameof(StateVM)] as StateVM;
+
+        /// <summary>
+        /// Requests current state or creates a new one.
+        /// </summary>
+        [Route("state")]
+        [HttpGet]
+        public StateVM GetState()
+        {
+            if (State == null)
+                HttpContext.Current.Session[nameof(StateVM)] = _stateManager.CreateNewState();
+
+            return State;
+        }
         
         /// <summary>
         /// Checks an identification attempt.
@@ -26,8 +43,10 @@ namespace FaceGame.Controllers
         [HttpPost]
         public IdentificationResponseVM Identify(IdentificationVM request)
         {
-            // todo
-            return new IdentificationResponseVM();
+            if (State == null)
+                return null;
+
+            return _stateManager.Identificate(State, request);
         }
 
         /// <summary>
@@ -37,8 +56,11 @@ namespace FaceGame.Controllers
         [HttpPost]
         public int Complete(string name)
         {
-            // todo
-            return 1;
+            if (State == null)
+                return 0;
+
+            _hiscoreManager.AddHiscore(name, State.Score);
+            return State.Score;
         }
     }
 }
